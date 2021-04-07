9f2153bfd5a491e9f92fae7abcf49691dc772463
Dominic Chiampi <dominicchiampi@gmail.com>
2021-04-06 23:35:43 -0400

    got dev-in-docker working for my first project

---
+++ fate-dice/src/fate_dice/core.cljs
@@ -0,0 +1,19 @@
+(ns fate-dice.core
+  (:require
+   [reagent.core :as r]
+   [reagent.dom :as d]))
+
+;; -------------------------
+;; Views
+
+(defn home-page []
+  [:div [:h2 "Fate Dice Edit"]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (d/render [home-page] (.getElementById js/document "app")))
+
+(defn ^:export init! []
+  (mount-root))
